4684b43e1e9e4d6b81ee796f87f50f6460b8b031
Chien-Yu Huang <chienyuhuang@kkbox.com>
2016-03-29 10:19:20 +0800

    Init

---
+++ HW1.py
@@ -0,0 +1,18 @@
+import random
+
+class NeuralNetwork:
+    LEARNING_RATE = 0.5
+
+    def __init__(self):
+        self.output_layer
+        self.hidden_layer
+
+class NeuralLayer:
+
+    def __init__(self, biases: [float]):
+        self.biases = biases
+
+class Neuron:
+
+    def __init__(self, bias: int):
+        self.bias = bias
